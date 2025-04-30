8217264411f9d1961da8670d2ef644ab199fb753
Sagar Chethan Kumar <sagar.chethankumar@gmail.com>
2025-04-30 18:02:52 +0200

    fix redirect

--- components/qualtrics-button.tsx
+++ components/qualtrics-button.tsx
@@ -7,7 +7,7 @@ export interface QualtricsButtonProps {
 
 export function QualtricsButton({ uid, studyId }: QualtricsButtonProps) {
   return (
-    <Button onClick={() => window.open(`https://tudelft.fra1.qualtrics.com/jfe/form/SV_3EIr3zqrmkRzExM&uid=${uid}&study_id=${studyId}`, '_blank')}>
+    <Button onClick={() => window.open(`https://tudelft.fra1.qualtrics.com/jfe/form/SV_3EIr3zqrmkRzExM?uid=${uid}&study_id=${studyId}`, '_blank')}>
       Continue to Qualtrics
     </Button>
   )
