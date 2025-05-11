4885378baa79fff64f87cebefc8c52b8865b2b20
33an <y.shan-4@student.tudelft.nl>
2025-05-11 15:54:46 +0200

    fix yushan instruction

--- components/task-instructions/yushan-task-instructions.tsx
+++ components/task-instructions/yushan-task-instructions.tsx
@@ -8,13 +8,16 @@ export default function YushanTaskInstructions() {
           <p><strong>Scenario</strong></p>
           <hr />
           <p>
-              Imagine you are a bachelor student at risk of expulsion due to not earning enough credits this academic year. This situation has become a major source of stress, affecting your mental well-being and making it hard to focus on daily tasks, including your studies.
+              Imagine you are a bachelor student at risk of expulsion due to not earning enough credits this academic
+              year. This situation has become a major source of stress, affecting your mental well-being and making it
+              hard to focus on daily tasks, including your studies.
           </p>
           <hr />
           <p><strong>Task</strong></p>
           <hr />
           <p>
-              You will interact with <strong>four different versions</strong> of a mental health support chatbot. Please respond to each chatbot as if you were the student described in the scenario.
+              You will interact with <strong>four different versions</strong> of a mental health support chatbot.
+              Please respond to each chatbot as if you were the student described in the scenario.
           </p>
 
           <ul>
