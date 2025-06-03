50648e214033da5925934d57286578f7f469dd2d
Manu Gautam <m.gautam@student.tudelft.nl>
2025-06-03 11:41:44 +0200

    final changes

--- app/7a793cf6-19f8-4066-afe0-a009ce48e996/txtToAudio.py
+++ app/7a793cf6-19f8-4066-afe0-a009ce48e996/txtToAudio.py
@@ -1,6 +1,7 @@
 from gtts import gTTS
 import json
 import os
+from pydub import AudioSegment
 
 # This function takes a list of strings (text) and converts each of them to audio and saves them as mp3 files.
 def text_to_mp3():
@@ -45,10 +46,25 @@ def text_to_mp3():
 if __name__ == "__main__":
     # Example usage
     # text_to_mp3()
-    message = "In this scenario, lets talk about personal interests and how they impact our well-being."
+    message = """
+We are committed to safeguarding personal data in compliance with international privacy standards such as the General Data Protection Regulation (GDPR). We ensure transparency in our data practices, guarantee that personal data is never sold to advertisers, and commit to storing data anonymously whenever possible.
+We collect personal data such as users’ willingness to self-disclose and their perceived sensitivity of questions. This data is gathered with specific intent—to support academic research in developing mobile health (mHealth) chatbots and to personalize and improve the user experience with the Services.
+Personal data may be shared exclusively for academic and research purposes. All shared data is de-identified or anonymized to ensure that individual user identities remain protected.
+We use GDPR-compliant data servers to securely store personal data. Collected data is later transferred and stored anonymously on Surf Drive or TU Delft’s secure storage infrastructure.
+Personal data is retained only for as long as needed to fulfill the purposes for which it was collected, including supporting research and delivering services. Users may revoke their consent at any time, at which point all collected data will be voided and deleted in accordance with applicable regulations.
+Users have the right to access, correct, and delete their data. They may also revoke consent, restrict processing, and request a copy of their data, as outlined by data protection laws.
+Users can withdraw consent at any time, which will result in the deletion and invalidation of all previously collected data.
+Users are encouraged to regularly review this privacy policy to stay informed about how their data is handled.
+For questions or concerns regarding personal data, users can contact e.c.s.degroot@tudelft.nl.
+    """
 
     #Convert to audio
     audio = gTTS(text=message, lang='en', slow=False)
     # Save the audio file
-    audio.save("../../public/audioFilesManu/Scenario1/Scenario1-Step1.mp3")
+    audio.save("../../public/audioFilesManu/privacyPolicy.mp3")
+    privacy = AudioSegment.from_file("../../public/audioFilesManu/privacyPolicy.mp3")
+    faster_privacy = privacy.speedup(playback_speed=1.5)
+
+# Export the faster audio
+    faster_privacy.export("../../public/audioFilesManu/privacyPolicy_Fast.mp3", format="mp3")
     print("Audio files created successfully.")
